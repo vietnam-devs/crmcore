5dd8df026a9e9fbca628e6a09c9cd56ad71cee5e
Phuong Le <phuongle@Phuongs-MacBook-Pro.local>
2017-11-21 22:48:34 +0700

    implement login page

---
+++ src/modules/CRMCore.Module.Identity/ViewModels/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CRMCore.Module.Identity.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+}
