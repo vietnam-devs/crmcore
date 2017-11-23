36a4208d0d59f46104f4ca105dda97b4004e9c27
Phuong Le <tungphuongld@gmail.com>
2017-11-23 19:33:04 +0700

    support migration

---
+++ src/hosts/CRMCore.DBMigration.Console/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using IdentityServer4.EntityFramework.DbContexts;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace CRMCore.DBMigration.Console
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args)
+                .MigrateDbContext<PersistedGrantDbContext>((_, __) => { });
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>()
+                .ConfigureAppConfiguration((hostContext, options) =>
+                {
+                    options.Sources.Clear();
+                    options.AddJsonFile("appsettings.json", true, true);
+                    options.AddJsonFile($"appsettings.{hostContext.HostingEnvironment.EnvironmentName}.json", true);
+                    if (hostContext.HostingEnvironment.IsDevelopment())
+                    {
+                        options.AddUserSecrets<Startup>();
+                    }
+                    options.AddEnvironmentVariables();
+                })
+                .Build();
+    }
+}
---
+++ src/hosts/CRMCore.DBMigration.Console/Startup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace CRMCore.DBMigration.Console
+{
+    public class Startup
+    {
+        private IConfiguration Configuration { get; }
+
+        private IHostingEnvironment Environment { get; }
+
+        public Startup(IHostingEnvironment env, IConfiguration config)
+        {
+            Environment = env;
+            Configuration = config;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Adds IdentityServer             services.AddIdentityServer(x => x.IssuerUri = "null")                     .AddConfigurationStore(options =>                     {                         options.ConfigureDbContext = builder => builder.UseMySql(Configuration.GetConnectionString("Default"), mySqlOptionsAction: sqlOptions =>                             {                                 sqlOptions.MigrationsAssembly("CRMCore.DBMigration.Console");                             });                     })                 .AddOperationalStore(options =>                 {                     options.ConfigureDbContext = builder => builder.UseMySql(Configuration.GetConnectionString("Default"), mySqlOptionsAction: sqlOptions =>                     {                         sqlOptions.MigrationsAssembly("CRMCore.DBMigration.Console");                     });                 });
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+
+        }
+    }
+}
