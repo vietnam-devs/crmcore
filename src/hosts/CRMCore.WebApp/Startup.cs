5fefcf98fb475ee8da6ed39a82e102ec4044bd7f
thangchung <thangchung@ymail.com>
2017-12-06 20:36:52 +0700

    BE: refactor code

--- src/hosts/CRMCore.WebApp/Startup.cs
+++ src/hosts/CRMCore.WebApp/Startup.cs
@@ -1,11 +1,6 @@
 ﻿using AspNetCore.RouteAnalyzer;
-using Autofac;
-using Autofac.Extensions.DependencyInjection;
-using CRMCore.Framework.Entities;
+using CRMCore.Application.Crm.targets;
 using CRMCore.Framework.MvcCore.Extensions;
-using CRMCore.Module.Data;
-using CRMCore.Module.Data.Impl;
-using CRMCore.Module.Identity.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -13,49 +8,12 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
-using System.Linq;
 using System.Reflection;
-using System.Text.Encodings.Web;
-using Microsoft.Extensions.Logging;
 
 namespace CRMCore.WebApp
 {
-    public static class Constants
-    {
-        public static readonly string ApiPrefix = "/api";
-        public static readonly string IdentityPrefix = "/idsrv";
-    }
-
-    public static class ServiceCollectionExtensions
-    {
-        public static IServiceProvider InitServices(this IServiceCollection services, IConfiguration configuration)
-        {
-            var builder = new ContainerBuilder();
-
-            builder.RegisterGeneric(typeof(EfRepositoryAsync<>))
-                .As(typeof(IEfRepositoryAsync<>));
-
-            builder.Register(x => new EfUnitOfWork(
-                    x.Resolve<ApplicationDbContext>(),
-                    x.Resolve<IServiceProvider>()))
-                .As(typeof(IUnitOfWorkAsync))
-                .SingleInstance();
-
-            builder.Populate(services);
-            return builder.Build().Resolve<IServiceProvider>();
-        }
-    }
-
     public class Startup
     {
-        private static readonly string[] IdSrvPaths =
-        {
-            "/client-callback-popup",
-            "/client-callback-silent",
-            "/account",
-            "/error"
-        };
-
         private IConfiguration Configuration { get; }
 
         private IHostingEnvironment Environment { get; }
@@ -68,113 +26,39 @@ namespace CRMCore.WebApp
 
         public IServiceProvider ConfigureServices(IServiceCollection services)
         {
-            var connectionString = Configuration.GetConnectionString("Default");
-            var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
-
-            services.AddSingleton(JavaScriptEncoder.Default);
-
-            services.AddDbContext<ApplicationDbContext>(options => options.UseMySql(connectionString));
-
-            services.AddMvcModules();
-            services.AddRouteAnalyzer();
-
-            services.RegisterIdentityAndID4(
-                Configuration.GetSection("Certificate"),
-                options =>
-                {
-                    options.ConfigureDbContext = builder =>
-                        builder.UseMySql(connectionString, sqlOptions =>
+            return services
+                .AddCrmCore(
+                    Configuration, 
+                    builder => builder.UseMySql(
+                        Configuration.GetConnectionString("Default"), 
+                        sqlOptions =>
                         {
-                            sqlOptions.MigrationsAssembly(migrationsAssembly);
-                        });
-                },
-                options =>
-                {
-                    options.ConfigureDbContext = builder =>
-                        builder.UseMySql(connectionString, sqlOptions =>
-                        {
-                            sqlOptions.MigrationsAssembly(migrationsAssembly);
-                        });
-                });
-
-            return services.InitServices(Configuration);
+                            sqlOptions.MigrationsAssembly(
+                                typeof(Startup).GetTypeInfo().Assembly.GetName().Name);
+                        }))
+                .AddRouteAnalyzer()
+                .BuildServiceProvider(false);
         }
 
         public void Configure(IApplicationBuilder app)
         {
             app.UseStaticFiles();
 
-            //Todo: need to refactor code, need to use authentication before MVC
+            if (Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
             app.UseAuthentication();
             app.UseIdentityServer();
 
-            MapAndUseIdSrv(app);
-            MapAndUseWebApp(app);
-            MapAndUseFrontend(app);
+            app.UseCors("CorsPolicy");
 
-            // TODO: consider moving this up
-            app.UseModules();
-        }
-
-        private void MapAndUseIdSrv(IApplicationBuilder app)
-        {
-            app.Map(Constants.IdentityPrefix, identityApp =>
-            {
-                if (Environment.IsDevelopment())
-                {
-                    identityApp.UseDeveloperExceptionPage();
-                }
-                else
-                {
-                    identityApp.UseExceptionHandler("/error");
-                }
-
-                // TODO: aPhuong will add more configurations for IdSrv 
-                // TODO: ...
-
-                /*identityApp.MapWhen(x => IsIdentityRequest(x), mvcApp =>
-                {
-                    mvcApp.UseMvc();
-                });*/
-            });
-        }
-
-        private void MapAndUseWebApp(IApplicationBuilder app)
-        {
-            app.Map(Constants.ApiPrefix, appApi =>
-            {
-                if (Environment.IsDevelopment())
-                {
-                    appApi.UseDeveloperExceptionPage();
-                }
-                else
-                {
-                    appApi.UseExceptionHandler("/Home/Error");
-                }
-
-                app.UseCors("CorsPolicy");
-
-                /*appApi.MapWhen(x => !IsIdentityRequest(x), webApp =>
-                {
-                    // TODO: Thang will map Swagger here
-                    // TODO: ...
-
-                    webApp.UseMvc(routes =>
-                        {
-                            routes.MapRoute(
-                            name: "default",
-                            template: "{controller=Home}/{action=Index}/{id?}");
-
-                            routes.MapSpaFallbackRoute(
-                                name: "spa-fallback",
-                                defaults: new { controller = "Home", action = "Index" });
-                        });
-                });*/
-            });
-        }
-
-        private void MapAndUseFrontend(IApplicationBuilder app)
-        {
+            // override / route :((
             app.Use((context, next) =>
             {
                 if (context.Request.Path.Value == "/")
@@ -192,16 +76,10 @@ namespace CRMCore.WebApp
                     name: "default",
                     template: "{controller}/{action}/{id?}",
                     defaults: new { controller = "Home", action = "Index" });
-
-                routes.MapSpaFallbackRoute(
-                    name: "spa-fallback",
-                    defaults: new { controller = "Home", action = "Index" });
             });
-        }
 
-        private static bool IsIdentityRequest(HttpContext context)
-        {
-            return IdSrvPaths.Any(p => context.Request.Path.StartsWithSegments(p));
+            // TODO: consider moving this up
+            app.UseModules();
         }
     }
 }
---
+++ src/targets/CRMCore.Application.Crm.targets/ServiceExtensions.cs
@@ -0,0 +1,63 @@
+﻿using CRMCore.Framework.Entities;
+using CRMCore.Framework.MvcCore.Extensions;
+using CRMCore.Module.Data;
+using CRMCore.Module.Data.Impl;
+using CRMCore.Module.Identity.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Text.Encodings.Web;
+
+namespace CRMCore.Application.Crm.targets
+{
+    public static class ServiceExtensions
+    {
+        public static IServiceCollection AddCrmCore(
+            this IServiceCollection services, 
+            IConfiguration config, 
+            Action<DbContextOptionsBuilder> dbContextOptionsBuilderAction)
+        {
+            services.AddSingleton(JavaScriptEncoder.Default);
+
+            services.AddDbContext<ApplicationDbContext>(options => dbContextOptionsBuilderAction(options));
+
+            services.AddMvcModules();
+
+            services.RegisterIdentityAndID4(
+                config.GetSection("Certificate"),
+                options =>
+                {
+                    options.ConfigureDbContext = builder =>
+                        dbContextOptionsBuilderAction(builder);
+                },
+                options =>
+                {
+                    options.ConfigureDbContext = builder => dbContextOptionsBuilderAction(builder);
+                });
+
+            // TODO: I think this one should move to CRMCore.Module.Data :))
+            var entityTypes = "CRMCore.Module.*".LoadAssemblyWithPattern()
+                .SelectMany(m => m.DefinedTypes)
+                .Where(x => typeof(IEntity)
+                .IsAssignableFrom(x) && !x.GetTypeInfo().IsAbstract);
+
+            foreach (var entity in entityTypes)
+            {
+                var repoType = typeof(IEfRepositoryAsync<>).MakeGenericType(entity);
+                var implRepoType = typeof(EfRepositoryAsync<>).MakeGenericType(entity);
+                services.AddSingleton(repoType, implRepoType);
+            }
+
+            services.AddSingleton(
+                typeof(IUnitOfWorkAsync), resolver =>
+                new EfUnitOfWork(
+                    resolver.GetService<ApplicationDbContext>(),
+                    resolver.GetService<IServiceProvider>()));
+
+            return services;
+        }
+    }
+}
